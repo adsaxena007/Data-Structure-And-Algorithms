e7d08ddcb572c8bd2a091426e7ba24a78691becc
adityasaxena <aditya.saxena.6991@gmail.com>
2024-08-26 11:05:46 +0530

    _37 to _43

---
+++ src/_37_RansomNote.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class _37_RansomNote {
+
+    Map<Character, Integer> wordCount;
+    public boolean canConstruct(String ransomNote, String magazine) {
+        wordCount = new HashMap<>();
+        for(Character ch : ransomNote.toCharArray()){
+            wordCount.put(ch, wordCount.getOrDefault(ch,0) + 1);
+        }
+        Map<Character, Integer> currentMap = new HashMap<>();
+        int formed = 0;
+        int i = 0;
+        boolean canBeConstructed = false;
+        while(i<magazine.length()){
+            Character key = magazine.charAt(i);
+            if(wordCount.containsKey(key)){
+                currentMap.put(key, currentMap.getOrDefault(key,0) + 1);
+                if(currentMap.get(key).equals(wordCount.get(key))){
+                    formed++;
+                }
+            }
+
+            if(formed == wordCount.size()){
+                canBeConstructed = true;
+                break;
+            }
+            i++;
+        }
+        return canBeConstructed;
+    }
+
+    public static void main(String[] args) {
+        _37_RansomNote ob = new _37_RansomNote();
+        System.out.println(ob.canConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+    }
+}
